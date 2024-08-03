1c1592749ab965b65b87d4c6bb284b70c32874b2
adminmetricvisio <admin@metricvisio.org>
2024-08-03 13:14:39 -0700

    Updating Bot

--- AIAgent.py
+++ AIAgent.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
 
-API_KEY = "YOUR_API_KEY"
+
+API_KEY = "AIzaSyD1weJf-5aApL2J1EJ9y9wcAnYf5xqG11Q"
 
 genai.configure (
     api_key=API_KEY
@@ -13,10 +14,10 @@ chat = model.start_chat(history=[])
 while (True):
     question = input("You: ")
 
-    if (question.strip() == ''):
+    if (question.strip() == 'exit'):
         break
 
     response = chat.send_message(question)
     print("\n")
-    print(f"Bot: {response.text}")
+    print(f"Bot (Exit To Quit): {response.text}")
     print('\n')
